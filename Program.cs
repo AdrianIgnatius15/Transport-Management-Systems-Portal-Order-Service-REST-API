c354b8c8fa95649d94fc84d22cb9ffb751755794
Adrian Joseph <adrianignatius15@hotmail.com>
2026-04-26 00:08:52 +0800

    Fix potential bug on circular references

--- Program.cs
+++ Program.cs
@@ -58,7 +58,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 builder.Services.AddAuthorization();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
